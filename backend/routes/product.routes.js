efd17193e9cb0e7065c6261d3d25fea02fb3e511
nithi-netizen <23ce045@kpriet.ac.in>
2026-01-15 03:55:29 +0530

    Add/update product routes

--- backend/routes/product.routes.js
+++ backend/routes/product.routes.js
@@ -1,15 +1,8 @@
 const express = require("express");
 const router = express.Router();
 
-const {
-  getProducts,
-} = require("../controllers/products.controller");
+const { getProducts } = require("../controllers/products.controller");
 
-/**
- * =========================
- * PUBLIC PRODUCT ROUTES
- * =========================
- */
 router.get("/", getProducts);
 
 module.exports = router;
