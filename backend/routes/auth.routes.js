a99952deeb8b0106f2b51b46e2a7aa03a75432fe
nithi-netizen <23ce045@kpriet.ac.in>
2026-01-16 03:28:25 +0530

    Fix admin auth, dashboard, orders, revenue, and routes

--- backend/routes/auth.routes.js
+++ backend/routes/auth.routes.js
@@ -2,6 +2,8 @@ const express = require("express");
 const router = express.Router();
 const jwt = require("jsonwebtoken");
 const nodemailer = require("nodemailer");
+const sendOTPEmail = require("../utils/sendEmail");
+
 
 /* OTP STORE */
 const otpStore = new Map();
